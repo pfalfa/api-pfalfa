8f31572b6f8d62da42a86528c7a376ec9f3d70ec
eksant <eksant@gmail.com>
2019-11-10 21:11:51 +0800

    Initial master

---
+++ config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  app: {
+    port: process.env.PORT,
+    host: 'localhost',
+    mainRoute: '/api',
+    modeServer: 'http',
+    modeCluster: false,
+    openSslKeyPath: process.env.SSL_KEYPATH || null,
+    openSslCertPath: process.env.SSL_CERTPATH || null,
+    loggerFilePath: './logs/access.log',
+    rateLimitSuspendTime: 5,
+    rateLimitMaxHitPerIP: 500,
+    pageLimit: 10,
+  },
+  db: {
+    region: 'ap-southeast-1',
+    tableName: process.env.TABLE_NAME,
+    accessKeyId: process.env.ACCESS_KEY_ID,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY,
+  },
+}
---
+++ index.js
@@ -0,0 +1,69 @@
+require('dotenv').config()
+const fs = require('fs')
+const os = require('os')
+const cors = require('cors')
+const http = require('http')
+const AWS = require('aws-sdk')
+const https = require('https')
+const logger = require('morgan')
+const helmet = require('helmet')
+const cluster = require('cluster')
+const express = require('express')
+const bodyParser = require('body-parser')
+const compression = require('compression')
+const session = require('express-session')
+
+const config = require('./config')
+const routes = require('./routes')
+const app = express().set('port', config.app.port)
+
+AWS.config.update(config.db)
+
+/** express server */
+app.use(cors())
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: false }))
+app.use(bodyParser.json({ limit: '30mb', extended: false }))
+app.use(helmet())
+app.use(compression())
+app.use(logger('dev'))
+app.use(session({ secret: 'blessedecoliving', resave: false, saveUninitialized: true, cookie: { maxAge: 60000 } }))
+
+/** router */
+routes(app)
+
+/** cluster server */
+const server =
+  config.app.modeServer === 'http'
+    ? http.createServer(app)
+    : https.createServer(
+        {
+          key: fs.readFileSync(config.app.openSslKeyPath),
+          cert: fs.readFileSync(config.app.openSslCertPath),
+        },
+        app
+      )
+if (cluster.isMaster && config.app.modeCluster) {
+  const cpus = os.cpus().length
+  for (let i = 0; i < cpus; i++) {
+    cluster.fork()
+  }
+  console.log(`Mode Cluster. Forking for ${cpus} CPUs`)
+} else {
+  const port = config.app.port
+  server.listen(port, () => {
+    console.log(`Start Express Server on Port ${port} Handled by Process ${process.pid}`)
+    return
+  })
+
+  process.on('SIGINT', () => {
+    server.close(err => {
+      if (err) {
+        console.error(`Error Express Server : ${err}`)
+        process.exit(1)
+        return
+      }
+      console.log(`Close Express Server on Port ${port} Handled by Process ${process.pid}`)
+      process.exit(0)
+    })
+  })
+}
