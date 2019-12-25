4f99d8d5d4127ea4fb42d4a38052273ec3179609
eksant <eksant@gmail.com>
2019-12-26 03:00:56 +0800

    Update schema dapps

--- controllers/dapps.js
+++ controllers/dapps.js
@@ -20,7 +20,7 @@ const getById = (req, res) => {
 
     const dappUploads =
       data && data.ipfsHash
-        ? await api.get(`ipfs/list/${data.ipfsHash}`).then(resp => {
+        ? await api.get(api.host.dev, `ipfs/list/${data.ipfsHash}`).then(resp => {
             return resp.data.length > 0 ? _.sortBy(resp.data, ['Type', 'Name']) : null
           })
         : []
@@ -30,35 +30,32 @@ const getById = (req, res) => {
 }
 
 const created = (req, res) => {
-  const { logo_url, pod_name, category, description } = req.body
-  if (!pod_name || !category) return res.status(400).json({ success: false, message: 'Invalid payload', data: null, paginate: null })
+  const { logoUrl, podName, category, description } = req.body
+  if (!podName || !category) return res.status(400).json({ success: false, message: 'Invalid payload', data: null, paginate: null })
 
+  const payload = { podName, port: 80 }
   api
-    .post('dapps/create', { pod_name, port: 80 })
+    .post(api.host.dev, 'dapps/create', payload)
     .then(resp => {
       const { status, error_msg, data } = resp
       if (status === 'failed') return res.status(400).json({ success: false, message: error_msg, data: null, paginate: null })
 
-      const dappStatus = data.items[0].status
-      if (dappStatus && dappStatus.conditions && dappStatus.conditions[0].status === 'False') {
-        return res.status(400).json({ success: false, message: dappStatus.conditions[0].message, data: null, paginate: null })
-      }
-
       const { apiVersion, metadata, spec } = data.items[0]
       const item = {
-        dappUid: metadata.uid,
-        dappCreated: metadata.creationTimestamp,
-        dappDetail: data.items[0],
-        logoUrl: logo_url || null,
-        name: metadata.name,
-        port: spec.containers[0].ports[0].containerPort,
-        pubkey: req.UserAuth.pub,
-        IpPublic: dappStatus.hostIP,
-        IpLocal: dappStatus.podIP,
-        phase: dappStatus.phase,
         category,
         apiVersion,
         description,
+        ipLocal: null,
+        status: null,
+        port: payload.port,
+        name: metadata.name,
+        dappUid: metadata.uid,
+        pubkey: req.UserAuth.pub,
+        dappDetail: data.items[0],
+        logoUrl: logoUrl || null,
+        dappCreated: metadata.creationTimestamp,
+        gunDb: `${spec.clusterIP}:${spec.ports[0].port}/gun`,
+        ipPublic: spec.loadBalancer && spec.loadBalancer.ingress ? spec.loadBalancer.ingress[0].ip : null,
       }
       const dapps = new Dapps(item)
       dapps
@@ -99,11 +96,21 @@ const deleted = (req, res) => {
     if (err) return res.status(500).json({ success: false, message: err, data: null, paginate: null })
     if (!data || data.isDeleted) return res.status(400).json({ success: false, message: 'Dapp not found', data: null, paginate: null })
 
-    const item = { isDeleted: true }
-    Dapps.update({ id: filter.id }, item, (err, data) => {
-      if (err) return res.status(500).json({ status: 500, success: false, message: err, data: null, paginate: null })
-      return res.status(201).json({ success: true, message: 'Dapp deleted successfully', data, paginate: null })
-    })
+    api
+      .get(api.host.dev, `dapps/${data.name}/delete`)
+      .then(resp => {
+        const { status, error_msg } = resp
+        if (status === 'failed') return res.status(400).json({ success: false, message: error_msg, data: null, paginate: null })
+
+        const item = { isDeleted: true }
+        Dapps.update({ id: filter.id }, item, (err, data) => {
+          if (err) return res.status(500).json({ status: 500, success: false, message: err, data: null, paginate: null })
+          return res.status(201).json({ success: true, message: 'Dapp deleted successfully', data, paginate: null })
+        })
+      })
+      .catch(error => {
+        return res.status(500).json({ success: true, message: error.message, data: null, paginate: null })
+      })
   })
 
   // const deleted = (req, res) => {
@@ -114,4 +121,4 @@ const deleted = (req, res) => {
   // }
 }
 
-module.exports = { getAll, getById, created, updated, deleted }
+module.exports = dapps = { getAll, getById, created, updated, deleted }
